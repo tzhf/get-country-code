df7fa674411c29a999d4be336af5a4f5e75a657f
tzhf <theo.zahaf@gmail.com>
2021-10-21 15:17:58 +0200

    added custom geojson

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import { readFile } from "fs/promises";
-const geojson = JSON.parse(await readFile(new URL("./data/countries.geojson", import.meta.url)));
-
+import countries from './data/countries.js';
 import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
 
 /**
@@ -8,9 +6,9 @@ import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
  * @returns {{code: String} | undefined} Object {country: String, code: String} | undefined
  */
 export function getCountryCode(point) {
-	for (let feature of geojson.features) {
+	for (let feature of countries.features) {
 		if (booleanPointInPolygon([point.lng, point.lat], feature)) {
-			return { country: feature.properties.country, code: feature.properties.ISO_A2 };
+			return { code: feature.properties.code, country: feature.properties.name };
 		}
 	}
 }
