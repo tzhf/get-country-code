5ddc1edbff526c5b99acfc6741647ef74863a976
tzhf <theo.zahaf@gmail.com>
2021-10-22 22:45:35 +0200

    fixed wrong return

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-import borders from "./data/borders.json";
+import borders from "./data/borders.js";
 import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
 
 /**
  * @param {{lat: Number, lng: Number}} point {lat, lng}
- * @returns {{code: String, name: String, country?: String} | 'none'} Object feature.properties | String: 'none'
+ * @returns {{code: String, name: String, country?: String} | {code: "none"}} Object feature.properties | Object {code: "none"}
  */
 export function getCountryCode(point) {
 	for (let feature of borders.features) {
-		if (booleanPointInPolygon([point.lng, point.lat], feature)) {
-			return feature.properties;
-		}
-		return "none";
+		if (booleanPointInPolygon([point.lng, point.lat], feature)) return feature.properties;
 	}
+	return { name: "none" };
 }
